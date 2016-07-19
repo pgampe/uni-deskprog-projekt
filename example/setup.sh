24d23bdcbac34022b339d2badb756cd57fb39777
Philipp Gampe <github.com@philippgampe.info>
2016-07-19 10:54:32 +0200

    Update setup.sh

--- example/setup.sh
+++ example/setup.sh
@@ -4,14 +4,15 @@ cd "$(dirname "$0")"
 set -e
 
 echo "before"
-ls
+find .
 
 elm-package install -y
 
-echo "after" ls
+echo "after"
+find .
 
-ELM_CONSOLE_VERSION_DIR="$(ls elm-stuff/packages/laszlopandy/elm-console/)"
-ELM_IO_PATH="elm-stuff/packages/laszlopandy/elm-console/$ELM_CONSOLE_VERSION_DIR/elm-io.sh"
+#ELM_CONSOLE_VERSION_DIR="$(ls elm-stuff/packages/laszlopandy/elm-console/)"
+#ELM_IO_PATH="elm-stuff/packages/laszlopandy/elm-console/$ELM_CONSOLE_VERSION_DIR/elm-io.sh"
 
 elm-make --yes --output example.html example.elm
 ##bash $ELM_IO_PATH raw-test.js test.js
